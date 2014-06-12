9c9c2e9ee5d84c5f767bf9e3274b5e622283b01b
Everett Moser <everettmoser@Everetts-MacBook-Pro.local>
2014-06-12 13:44:05 -0500

    Working on the collision functions
    Also first test run

--- androidpolygons/src/com/everettmoser/polygons/GameScreen.java
+++ androidpolygons/src/com/everettmoser/polygons/GameScreen.java
@@ -16,29 +16,56 @@ import com.kilobolt.framework.Screen;
 public class GameScreen extends Screen 
 {
 
-	// Variable Setup
+	Polygon p1, p2;
 
 	public GameScreen(Game game) {
 		super(game);
 
 		// Initialize game objects here
-
+		
+		p1 = new Polygon(new Vector2D(100, 100));
+		p2 = new Polygon(new Vector2D(200, 100));
+		
+		p1.addVertex(new Vector2D(0, -60));
+		p1.addVertex(new Vector2D((float)(-90 / Math.sqrt(3)), 30));
+		p1.addVertex(new Vector2D((float)(90 / Math.sqrt(3)), 30));
+		
+		p2.addVertex(new Vector2D(0, 60));
+		p2.addVertex(new Vector2D((float)(-90 / Math.sqrt(3)), -30));
+		p2.addVertex(new Vector2D((float)(90 / Math.sqrt(3)), -30));
 	}
 
 	
 	@Override
 	public void update(float deltaTime) {
 		List<TouchEvent> touchEvents = game.getInput().getTouchEvents();
-
+		p1.rotate((float)Math.PI / 100);
+		p2.rotate((float)Math.PI / 100);
+		
+		Polygon.collisionCheck(p1, p2);
+			
 	}
 
 
 	@Override
 	public void paint(float deltaTime) {
-
 		Graphics g = game.getGraphics();
 		g.clearScreen(Color.BLACK);
-		g.drawString("AccelX=" + game.getInput().getAccelerometerX(), 100, 100, new Paint());
+		Paint whitePaint = new Paint();
+		whitePaint.setColor(Color.WHITE);
+		
+		if (Polygon.collisionCheck(p1, p2))
+		{
+			p1.draw(g, Color.WHITE);
+			p2.draw(g, Color.WHITE);
+			g.drawString("COLLISION", 100, 400, whitePaint);
+		}
+		else
+		{
+			p1.draw(g, Color.CYAN);
+			p2.draw(g, Color.RED);
+			g.drawString("NO COLLISION", 100, 400, whitePaint);
+		}
 	}
 
 	private void nullify() {
--- androidpolygons/src/com/everettmoser/polygons/Polygon.java
+++ androidpolygons/src/com/everettmoser/polygons/Polygon.java
@@ -1,6 +1,10 @@
 package com.everettmoser.polygons;
 
 import java.util.ArrayList;
+import android.graphics.Color;
+import android.util.Log;
+
+import com.kilobolt.framework.Graphics;
 
 public class Polygon 
 {
@@ -22,6 +26,8 @@ public class Polygon
 		{
 			pts.get(i).setX(newVertex.getX());
 			pts.get(i).setY(newVertex.getY());
+			if (radius < Vector2D.distance(newVertex))
+				radius = Vector2D.distance(newVertex);
 		}
 	}
 	
@@ -40,6 +46,8 @@ public class Polygon
 	{
 		pts.add(new Vector2D(newVertex));
 		size ++;
+		if (radius < Vector2D.distance(newVertex))
+			radius = Vector2D.distance(newVertex);
 	}
 	
 	public void addVertexIndex(int index, Vector2D newVertex)
@@ -48,6 +56,9 @@ public class Polygon
 			pts.add(index, new Vector2D(newVertex));
 		else
 			pts.add(new Vector2D(newVertex));
+
+		if (radius < Vector2D.distance(newVertex))
+			radius = Vector2D.distance(newVertex);
 		size ++;
 	}
 	
@@ -57,6 +68,12 @@ public class Polygon
 		{
 			pts.remove(index);
 			size --;
+			radius = 0.f;
+			for (int i = 0; i < size; i ++)
+			{
+				if (Vector2D.distance(pts.get(i)) > radius)
+					radius = Vector2D.distance(pts.get(i));
+			}
 		}
 	}
 	
@@ -66,6 +83,12 @@ public class Polygon
 		{
 			pts.remove(pts.size() - 1);
 			size --;
+			radius = 0.f;
+			for (int i = 0; i < size; i ++)
+			{
+				if (Vector2D.distance(pts.get(i)) > radius)
+					radius = Vector2D.distance(pts.get(i));
+			}
 		}
 	}
 	
@@ -82,8 +105,8 @@ public class Polygon
 		{
 			px = pts.get(i).getX();
 			py = pts.get(i).getY();
-			nx = px * (float)Math.cos(theta) + py * (float)Math.sin(theta);
-			ny = px * (float)Math.sin(theta) - py * (float)Math.cos(theta);
+			nx = px * (float)Math.cos(theta) - py * (float)Math.sin(theta);
+			ny = px * (float)Math.sin(theta) + py * (float)Math.cos(theta);
 			pts.get(i).setX(nx);
 			pts.get(i).setY(ny);
 		}
@@ -143,22 +166,137 @@ public class Polygon
 	
 	public static boolean collisionCheck(Polygon p1, Polygon p2)
 	{
+
+		Log.d("Polygon", "COLLISION CHECK " + (p1.getRadius() + p2.getRadius()) + " " + (Vector2D.distance(p1.getCenter(), p2.getCenter())));
 		// collision circle check
 		if ((p1.getRadius() + p2.getRadius()) < (Vector2D.distance(p1.getCenter(), p2.getCenter())))
 			return false;
 		
+		Log.d("Polygon", "Passed circular collision");
+		
 		// check lines
+		Vector2D pt = new Vector2D(0, 0);
+		float cl_x1, cl_x2, cl_x3, cl_x4, cl_y1, cl_y2, cl_y3, cl_y4;
+		float denom;
 		for (int i = 0; i < p1.getSize(); i ++)
 		{
 			for (int j = 0; j < p2.getSize(); j ++)
 			{
+				cl_x1 = p1.getVertex(i).getX();
+				cl_x2 = p1.getVertex((i + 1) % p1.getSize()).getX();
+				cl_y1 = p1.getVertex(i).getY();
+				cl_y2 = p1.getVertex((i + 1) % p1.getSize()).getY();
+				cl_x3 = p2.getVertex(j).getX();
+				cl_x4 = p2.getVertex((j + 1) % p2.getSize()).getX();
+				cl_y3 = p2.getVertex(j).getY();
+				cl_y4 = p2.getVertex((j + 1) % p2.getSize()).getY();
+				denom = ((cl_x1 - cl_x2) * (cl_y3 - cl_y4) - (cl_y1 - cl_y2) * (cl_x3 - cl_x4));
 				
+				// check for parallel lines
+				if (denom == 0)
+					break;
+				
+				// the point
+				pt.setX(((cl_x1 * cl_y2 - cl_y1 * cl_x2) * (cl_x3 - cl_x4) - (cl_x1 - cl_x2) * (cl_x3 * cl_y4 - cl_y3 * cl_y4)) / denom);
+				pt.setY(((cl_x1 * cl_y2 - cl_y1 * cl_x2) * (cl_y3 - cl_y4) - (cl_y1 - cl_y2) * (cl_x3 * cl_y4 - cl_y3 * cl_y4)) / denom);
+				
+				// check if point is on segment
+				if (pointOnLine(p1.getVertex(i), p1.getVertex((i + 1) % p1.getSize()), pt) 
+				 && pointOnLine(p2.getVertex(j), p2.getVertex((j + 1) % p2.getSize()), pt))
+					return true;
 			}
 		}
 		
+		Log.d("Polygon", "Passed Line Test");
+		/*
 		//check points
+		// p1 points in p2
+		float px1, px2, py1, py2;
+		Vector2D testPoint;
+		boolean flag1, flag2;
+		boolean inside = false;
 		
+		for (int i = 0; i < p1.getSize(); i ++)
+		{
+			px1 = p2.getVertex(p2.getSize() - 1).getX();
+			py1 = p2.getVertex(p2.getSize() - 1).getY();
+			testPoint = p1.getVertex(i);
+			inside = false;
+			flag1 = (testPoint.getY() >= py1);
+			
+			for (int j = 0; j < p2.getSize(); j ++)
+			{
+				px2 = p2.getVertex(j).getX();
+				py2 = p2.getVertex(j).getY();
+				flag2 = (testPoint.getY() >= py2);
+				if (flag1 != flag2)	
+				{
+					if (((py2 - testPoint.getY()) * (px1- - px2) >= (px2 - testPoint.getX()) * (py1 - py2)) == flag2 )
+						inside = !inside;
+				}
+				px1 = px2;
+				py1 = py2;
+				flag1 = flag2;
+			}
+			
+			if (inside) return true;
+		}
 		
+		Log.d("Polygon", "Passed p1 in p2");
+		
+		// p2 points in p1
+		for (int i = 0; i < p2.getSize(); i ++)
+		{
+			px1 = p1.getVertex(p1.getSize() - 1).getX();
+			py1 = p1.getVertex(p1.getSize() - 1).getY();
+			testPoint = p2.getVertex(i);
+			inside = false;
+			flag1 = (testPoint.getY() >= py1);
+			
+			for (int j = 0; j < p1.getSize(); j ++)
+			{
+				px2 = p1.getVertex(j).getX();
+				py2 = p1.getVertex(j).getY();
+				flag2 = (testPoint.getY() >= py2);
+				if (flag1 != flag2)	
+				{
+					if (((py2 - testPoint.getY()) * (px1- - px2) >= (px2 - testPoint.getX()) * (py1 - py2)) == flag2 )
+						inside = !inside;
+				}
+				px1 = px2;
+				py1 = py2;
+				flag1 = flag2;
+			}
+			
+			if (inside) return true;
+		}
+		*/
 		return false;
+	}
+	
+	private static boolean pointOnLine(Vector2D lineStart, Vector2D lineEnd, Vector2D point)
+	{
+		// assume point is on line, and we are checking wether or not it is on the particular segment
+		// primary use is for the collision function under "check lines"
+		float xmax, xmin, ymax, ymin;
+		xmax = Math.max(lineStart.getX(), lineEnd.getX());
+		xmin = Math.min(lineStart.getX(), lineEnd.getX());
+		ymax = Math.max(lineStart.getY(), lineEnd.getY());
+		ymin = Math.min(lineStart.getY(), lineEnd.getY());
+		
+		if (point.getX() > xmax) return false;
+		if (point.getX() < xmin) return false;
+		if (point.getY() > ymax) return false;
+		if (point.getY() < ymin) return false;
+		
+		return true;
+	}
+	
+	public void draw(Graphics g, int c)
+	{
+		for (int i = 0; i < size; i ++)
+		{
+			g.drawLine((int)(pts.get(i).getX() + center.getX()), (int)(pts.get(i).getY() + center.getY()), (int)(pts.get((i + 1) % size).getX() + center.getX()), (int)(pts.get((i + 1) % size).getY() + center.getY()), c);
+		}
 	}
 }
